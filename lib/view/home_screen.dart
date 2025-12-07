79b318795b9d42efc7d7e5be6bac256731aedc26
ahmedfyaz <awanahmed755@gmail.com>
2025-12-07 11:56:23 +0500

    feat: Display news headlines with images in home screen
    - Replace the placeholder `Container` in the `FutureBuilder` with a horizontal `ListView.builder`.
    - Use `CachedNetworkImage` to display article images from the API, improving performance and user experience.
    - Implement a `SpinKitFadingCircle` as a placeholder while images are loading.
    - Add an `Icon` to show an error state if an image fails to load.
    - Structure the layout using a `Stack` to display images within a styled `Container`.

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -15,8 +16,8 @@ class _HomeScreenState extends State<HomeScreen> {
   NewsViewModel newsViewModel = NewsViewModel();
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.sizeOf(context).width *1;
-    final height = MediaQuery.sizeOf(context).height *1;
+    final width = MediaQuery.sizeOf(context).width * 1;
+    final height = MediaQuery.sizeOf(context).height * 1;
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -34,24 +35,58 @@ class _HomeScreenState extends State<HomeScreen> {
           SizedBox(
             height: height * .55,
             width: width,
-            child: FutureBuilder<NewsChannelHeadlinesModel>(future: newsViewModel.fetchNewChannelHeadlinesApi(), builder: (BuildContext context, snapshot){
-              if(snapshot.connectionState == ConnectionState.waiting)
-                {
+            child: FutureBuilder<NewsChannelHeadlinesModel>(
+              future: newsViewModel.fetchNewChannelHeadlinesApi(),
+              builder: (BuildContext context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
                   return SpinKitFadingCircle(
                     size: 50,
                     color: Color(0xFFF6794D),
                   );
-                }
-              else
-                {
-                  return Container(
-                    color: Colors.grey,
+                } else {
+                  return ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: snapshot.data!.articles!.length,
+                    itemBuilder: (context, index) {
+                      return SizedBox(
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Container(
+                              height: height* .6,
+                              width: width* .9,
+                              padding: EdgeInsets.symmetric(
+                                horizontal: height * 0.02,
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(16),
+                                child: CachedNetworkImage(
+                                  imageUrl: snapshot
+                                      .data!
+                                      .articles![index]
+                                      .urlToImage
+                                      .toString(),
+                                  fit: BoxFit.cover,
+                                  placeholder: (context,url)=> SpinKitFadingCircle(
+                                    color: Colors.amber,
+                                    size: 50,
+                                  ),
+                                  errorWidget: (context,url,error)=>Icon(Icons.warning,color: Colors.redAccent,),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
                   );
                 }
-            }),
-          )
+              },
+            ),
+          ),
         ],
       ),
     );
   }
+
 }
