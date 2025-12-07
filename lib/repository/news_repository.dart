6c339b743645da3678f756cb180de82ebc045e5e
ahmedfyaz <awanahmed755@gmail.com>
2025-12-07 10:57:48 +0500

    Implemented Api

--- lib/repository/news_repository.dart
+++ lib/repository/news_repository.dart
@@ -1,7 +1,22 @@
+import "dart:convert";
+
+import "package:http/http.dart" as http;
+import "package:news_app/models/news_channel_headlines_model.dart";
+
 String API_KEY = "568de588371843c09f4f66260cb4cab6";
 
 class NewsRepository {
-  Future<void>fetchNewChannelHeadlinesApi()async{
-
+  Future<NewsChannelHeadlinesModel>fetchNewChannelHeadlinesApi()async{
+    String URL = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=$API_KEY";
+    final response = await http.get(Uri.parse(URL));
+    if(response.statusCode == 200)
+      {
+        final body = jsonDecode(response.body);
+        return NewsChannelHeadlinesModel.fromJson(body);
+      }
+    else
+      {
+        throw Exception('Error');
+      }
   }
 }
